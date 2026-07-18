2ab783bd08cadf133747ee37ede1c70552fe34ed
chaunguyenbinh01082007-cell <binh.chau5iu8@hcmut.edu.vn>
2026-07-18 21:58:41 +0700

    week04: hoan thanh bai tap buoi 4

--- week04/buoi04_runcodetask5-main.c/main.c
+++ week04/buoi04_runcodetask5-main.c/main.c
@@ -1,71 +1,67 @@
 #include "stm32f103xb.h"
 
-void GPIO_4Btn_4Led_Init(void);
-void GPIO_4Btn_4Led_Handler(void);
-
-int main(void)
-{
-    GPIO_4Btn_4Led_Init();
-
-    while (1)
-    {
-        GPIO_4Btn_4Led_Handler();
-    }
+void delay_ms(uint32_t ms) {
+    for (volatile uint32_t i = 0; i < ms * 4000; i++);
 }
 
-void GPIO_4Btn_4Led_Init(void)
-{
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN;
-    
-    volatile uint32_t dummy = RCC->APB2ENR;
-    (void)dummy;
+void delay_us(uint32_t us) {
+    for (volatile uint32_t i = 0; i < us * 4; i++);
+}
 
-    AFIO->MAPR = (AFIO->MAPR & ~AFIO_MAPR_SWJ_CFG) | AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
+int main(void) {
+    RCC->APB2ENR |= (1 << 0) | (1 << 2) | (1 << 3);
+    AFIO->MAPR = (AFIO->MAPR & ~(7 << 24)) | (2 << 24);
 
-    GPIOA->CRL &= ~(GPIO_CRL_MODE1 | GPIO_CRL_CNF1 | GPIO_CRL_MODE2 | GPIO_CRL_CNF2 | 
-                    GPIO_CRL_MODE3 | GPIO_CRL_CNF3 | GPIO_CRL_MODE4 | GPIO_CRL_CNF4);
-    GPIOA->CRL |= (GPIO_CRL_CNF1_1 | GPIO_CRL_CNF2_1 | GPIO_CRL_CNF3_1 | GPIO_CRL_CNF4_1);
+    GPIOA->CRL &= ~(0x000FFFF0);
+    GPIOA->CRL |=  (0x00088220);
     GPIOA->ODR |= (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
 
-    GPIOB->CRL &= ~(GPIO_CRL_MODE0 | GPIO_CRL_CNF0 | GPIO_CRL_MODE1 | GPIO_CRL_CNF1 | 
-                    GPIO_CRL_MODE3 | GPIO_CRL_CNF3 | GPIO_CRL_MODE4 | GPIO_CRL_CNF4);
-    GPIOB->CRL |= (GPIO_CRL_MODE0_1 | GPIO_CRL_MODE1_1 | GPIO_CRL_MODE3_1 | GPIO_CRL_MODE4_1);
+    GPIOB->CRL &= ~(0x000FF0FF);
+    GPIOB->CRL |=  (0x00022022);
     GPIOB->ODR &= ~((1 << 0) | (1 << 1) | (1 << 3) | (1 << 4));
-}
 
-void GPIO_4Btn_4Led_Handler(void)
-{
-    static uint8_t btn1_last = 1, btn2_last = 1, btn3_last = 1, btn4_last = 1;
-    uint8_t btn1_curr = (GPIOA->IDR & (1 << 1)) ? 1 : 0;
-    uint8_t btn2_curr = (GPIOA->IDR & (1 << 2)) ? 1 : 0;
-    uint8_t btn3_curr = (GPIOA->IDR & (1 << 3)) ? 1 : 0;
-    uint8_t btn4_curr = (GPIOA->IDR & (1 << 4)) ? 1 : 0;
+    GPIOB->ODR |= ((1 << 0) | (1 << 1) | (1 << 3) | (1 << 4));
+    delay_ms(1000);
+    GPIOB->ODR &= ~((1 << 0) | (1 << 1) | (1 << 3) | (1 << 4));
+    delay_ms(200);
 
-    if (btn1_curr == 0 && btn1_last == 1)
-    {
-        for (volatile int i = 0; i < 30000; i++);
-        if (!(GPIOA->IDR & (1 << 1))) GPIOB->ODR ^= (1 << 0);
+    uint8_t btn_state[4] = {1, 1, 1, 1};
+    uint8_t last_btn_state[4] = {1, 1, 1, 1};
+
+    while (1) {
+        GPIOA->ODR &= ~(1 << 1);
+        delay_us(10);
+        btn_state[0] = (GPIOA->IDR & (1 << 3)) ? 1 : 0;
+        btn_state[1] = (GPIOA->IDR & (1 << 4)) ? 1 : 0;
+        GPIOA->ODR |= (1 << 1);
+
+        GPIOA->ODR &= ~(1 << 2);
+        delay_us(10);
+        btn_state[2] = (GPIOA->IDR & (1 << 3)) ? 1 : 0;
+        btn_state[3] = (GPIOA->IDR & (1 << 4)) ? 1 : 0;
+        GPIOA->ODR |= (1 << 2);
+
+        if (last_btn_state[0] == 1 && btn_state[0] == 0) {
+            GPIOB->ODR ^= (1 << 0);
+            delay_ms(50);
+        }
+        if (last_btn_state[1] == 1 && btn_state[1] == 0) {
+            GPIOB->ODR ^= (1 << 1);
+            delay_ms(50);
+        }
+        if (last_btn_state[2] == 1 && btn_state[2] == 0) {
+            GPIOB->ODR ^= (1 << 3);
+            delay_ms(50);
+        }
+        if (last_btn_state[3] == 1 && btn_state[3] == 0) {
+            GPIOB->ODR ^= (1 << 4);
+            delay_ms(50);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            last_btn_state[i] = btn_state[i];
+        }
+
+        delay_ms(20);
     }
-    btn1_last = btn1_curr;
-
-    if (btn2_curr == 0 && btn2_last == 1)
-    {
-        for (volatile int i = 0; i < 30000; i++);
-        if (!(GPIOA->IDR & (1 << 2))) GPIOB->ODR ^= (1 << 1);
-    }
-    btn2_last = btn2_curr;
-
-    if (btn3_curr == 0 && btn3_last == 1)
-    {
-        for (volatile int i = 0; i < 30000; i++);
-        if (!(GPIOA->IDR & (1 << 3))) GPIOB->ODR ^= (1 << 3);
-    }
-    btn3_last = btn3_curr;
-
-    if (btn4_curr == 0 && btn4_last == 1)
-    {
-        for (volatile int i = 0; i < 30000; i++);
-        if (!(GPIOA->IDR & (1 << 4))) GPIOB->ODR ^= (1 << 4);
-    }
-    btn4_last = btn4_curr;
 }
